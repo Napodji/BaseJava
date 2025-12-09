b3c452983020d586fe0742bf310b63c3fdd78ad9
Napoji <sasha.k7@mail.ru>
2025-12-09 17:24:03 +0300

    - Use new Resume(uuid) constructor instead of new Resume() + setUuid() - Rename getIndex() → findIndex() - Rename insertElement() → insertResume() - Replace direct size access with size() getter:

--- src/com/basejava/webapp/storage/SortedArrayStorage.java
+++ src/com/basejava/webapp/storage/SortedArrayStorage.java
@@ -7,22 +7,21 @@ import java.util.Arrays;
 public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
-    protected int getIndex(String uuid) {
-        Resume searchKey = new Resume();
-        searchKey.setUuid(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+    protected int findIndex(String uuid) {
+        Resume searchKey = new Resume(uuid);
+        return Arrays.binarySearch(storage, 0, size(), searchKey);
     }
 
     @Override
-    protected void insertElement(Resume r, int index) {
+    protected void insertResume(Resume r, int index) {
         int insertPos = -index - 1;
-        System.arraycopy(storage, insertPos, storage, insertPos + 1, size - insertPos);
+        System.arraycopy(storage, insertPos, storage, insertPos + 1, size() - insertPos);
         storage[insertPos] = r;
     }
 
     @Override
     protected void fillDeletedElement(int index) {
-        int numMoved = size - index - 1;
+        int numMoved = size() - index - 1;
         if (numMoved > 0) {
             System.arraycopy(storage, index + 1, storage, index, numMoved);
         }
