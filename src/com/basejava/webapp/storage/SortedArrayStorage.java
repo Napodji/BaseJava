be1ab7c58b0ee6e48cb7d7885e62b85e03f7ce36
Napoji <sasha.k7@mail.ru>
2025-12-18 23:35:19 +0300

    Update SortedArrayStorage: use Resume constructor with uuid and replace size() with size field

--- src/com/basejava/webapp/storage/SortedArrayStorage.java
+++ src/com/basejava/webapp/storage/SortedArrayStorage.java
@@ -9,19 +9,19 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     protected int findIndex(String uuid) {
         Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size(), searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey);
     }
 
     @Override
     protected void insertResume(Resume r, int index) {
         int insertPos = -index - 1;
-        System.arraycopy(storage, insertPos, storage, insertPos + 1, size() - insertPos);
+        System.arraycopy(storage, insertPos, storage, insertPos + 1, size - insertPos);
         storage[insertPos] = r;
     }
 
     @Override
     protected void fillDeletedElement(int index) {
-        int numMoved = size() - index - 1;
+        int numMoved = size - index - 1;
         if (numMoved > 0) {
             System.arraycopy(storage, index + 1, storage, index, numMoved);
         }
