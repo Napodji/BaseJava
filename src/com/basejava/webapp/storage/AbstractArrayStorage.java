3114d2a87066d654655d99e5f2348cde49aaf238
Napoji <sasha.k7@mail.ru>
2025-12-20 13:55:05 +0300

    Refactor AbstractArrayStorage: apply guard clauses and reorder methods

--- src/com/basejava/webapp/storage/AbstractArrayStorage.java
+++ src/com/basejava/webapp/storage/AbstractArrayStorage.java
@@ -16,45 +16,16 @@ public abstract class AbstractArrayStorage implements Storage {
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
 
-    public int size() {
-        return size;
-    }
-
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
     public void save(Resume r) {
+        if (size >= STORAGE_LIMIT) {
+            throw new StorageException("Storage overflow", r.getUuid());
+        }
         int index = findIndex(r.getUuid());
         if (index >= 0) {
             throw new ExistStorageException(r.getUuid());
-        } else if (size >= STORAGE_LIMIT) {
-            throw new StorageException("Storage overflow", r.getUuid());
-        } else {
-            insertResume(r, index);
-            size++;
         }
-    }
-
-    public void delete(String uuid) {
-        int index = findIndex(uuid);
-        if (index < 0) {
-            throw new NotExistStorageException(uuid);
-        } else {
-            fillDeletedElement(index);
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
-    public void update(Resume r) {
-        int index = findIndex(r.getUuid());
-        if (index < 0) {
-            throw new NotExistStorageException(r.getUuid());
-        } else {
-            storage[index] = r;
-        }
+        insertResume(r, index);
+        size++;
     }
 
     public Resume get(String uuid) {
@@ -65,8 +36,35 @@ public abstract class AbstractArrayStorage implements Storage {
         return storage[index];
     }
 
+    public void delete(String uuid) {
+        int index = findIndex(uuid);
+        if (index < 0) {
+            throw new NotExistStorageException(uuid);
+        }
+        fillDeletedElement(index);
+        storage[size - 1] = null;
+        size--;
+    }
+
+    public void update(Resume r) {
+        int index = findIndex(r.getUuid());
+        if (index < 0) {
+            throw new NotExistStorageException(r.getUuid());
+        }
+        storage[index] = r;
+    }
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
     public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
+    }
+
+    public int size() {
+        return size;
     }
 
     protected abstract int findIndex(String uuid);
