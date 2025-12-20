fc688b5687c61910f781593063524cd24916bdba
Napoji <sasha.k7@mail.ru>
2025-12-20 13:54:13 +0300

    Reorder StorageException constructors: simpler overload first

--- src/com/basejava/webapp/exception/StorageException.java
+++ src/com/basejava/webapp/exception/StorageException.java
@@ -1,16 +1,19 @@
 package com.basejava.webapp.exception;
 
+/**
+ * Base exception for storage errors
+ */
 public class StorageException extends RuntimeException {
 
     private final String uuid;
 
+    public StorageException(String message) {
+        this(message, null);
+    }
+
     public StorageException(String message, String uuid) {
         super(message);
         this.uuid = uuid;
-    }
-
-    public StorageException(String message) {
-        this(message, null);
     }
 
     public String getUuid() {
