22e9d3541b5dc77c7721bd8367c1175aa2ed676d
Napoji <sasha.k7@mail.ru>
2025-12-24 22:48:58 +0300

    Implement ListStorage based on ArrayList

---
+++ src/com/basejava/webapp/storage/ListStorage.java
@@ -0,0 +1,59 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListStorage extends AbstractStorage {
+    private List<Resume> storage = new ArrayList<>();
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return storage.toArray(new Resume[0]);
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+
+    @Override
+    protected Object getSearchKey(String uuid) {
+        for (int i = 0; i < storage.size(); i++) {
+            if (storage.get(i).getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return searchKey != null;
+    }
+
+    @Override
+    protected void saveResume(Resume r, Object searchKey) {
+        storage.add(r);
+    }
+
+    @Override
+    protected Resume getResume(Object searchKey) {
+        return storage.get((int) searchKey);
+    }
+
+    @Override
+    protected void deleteResume(Object searchKey) {
+        storage.remove((int) searchKey);
+    }
+
+    @Override
+    protected void updateResume(Resume r, Object searchKey) {
+        storage.set((int) searchKey, r);
+    }
+}
