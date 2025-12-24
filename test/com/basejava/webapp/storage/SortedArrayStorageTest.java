67b93014e9e2b70dfa94385ff941f7142fdc6283
Napoji <sasha.k7@mail.ru>
2025-12-24 22:50:37 +0300

    Update SortedArrayStorageTest to extend AbstractArrayStorageTest

--- test/com/basejava/webapp/storage/SortedArrayStorageTest.java
+++ test/com/basejava/webapp/storage/SortedArrayStorageTest.java
@@ -1,7 +1,6 @@
 package com.basejava.webapp.storage;
 
 public class SortedArrayStorageTest extends AbstractArrayStorageTest {
-
     public SortedArrayStorageTest() {
         super(new SortedArrayStorage());
     }
