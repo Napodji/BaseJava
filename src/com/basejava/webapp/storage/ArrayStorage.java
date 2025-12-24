d0e1511beb32f68faf89dddbca0f4b2942960327
Napoji <sasha.k7@mail.ru>
2025-12-24 22:47:54 +0300

    Fix CheckStyle

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -6,7 +6,6 @@ import com.basejava.webapp.model.Resume;
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage {
-
     @Override
     protected int findIndex(String uuid) {
         for (int i = 0; i < size; i++) {
