bc93a48ec57ae742a2c764b1e548561c94a039a8
Napoji <sasha.k7@mail.ru>
2025-12-24 22:50:21 +0300

    Fix CheckStyle

--- src/com/basejava/webapp/MainReflection.java
+++ src/com/basejava/webapp/MainReflection.java
@@ -1,12 +1,10 @@
 package com.basejava.webapp;
 
 import com.basejava.webapp.model.Resume;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class MainReflection {
-
     public static void main(String[] args) throws Exception {
         Resume r = new Resume();
 
