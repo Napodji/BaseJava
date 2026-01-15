9a7228d4b7b7874026b9d7030f8ec0b5f35fc534
Napoji <sasha.k7@mail.ru>
2026-01-16 02:27:33 +0300

    Add MapResumeStorageTest

---
+++ test/com/basejava/webapp/storage/MapResumeStorageTest.java
@@ -0,0 +1,9 @@
+package com.basejava.webapp.storage;
+
+public class MapResumeStorageTest extends AbstractStorageTest {
+
+    public MapResumeStorageTest() {
+        super(new MapResumeStorage());
+    }
+}
+
