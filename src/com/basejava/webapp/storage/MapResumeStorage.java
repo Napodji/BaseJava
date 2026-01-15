5b476aeeba85a60a9e72906974dbb64a9bac9559
Napoji <sasha.k7@mail.ru>
2026-01-16 02:26:52 +0300

    Add MapResumeStorage with Resume as search key

---
+++ src/com/basejava/webapp/storage/MapResumeStorage.java
@@ -0,0 +1,62 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**.
+ * HashMap storage implementation (search key = Resume object)
+ */
+public class MapResumeStorage extends AbstractStorage {
+    private final Map<String, Resume> storage = new HashMap<>();
+
+    @Override
+    protected Resume getSearchKey(String uuid) {
+        return storage.get(uuid);
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return searchKey != null;
+    }
+
+    @Override
+    protected void saveResume(Resume r, Object searchKey) {
+        storage.put(r.getUuid(), r);
+    }
+
+    @Override
+    protected Resume getResume(Object searchKey) {
+        return (Resume) searchKey;
+    }
+
+    @Override
+    protected void deleteResume(Object searchKey) {
+        storage.remove(((Resume) searchKey).getUuid());
+    }
+
+    @Override
+    protected void updateResume(Resume r, Object searchKey) {
+        storage.put(r.getUuid(), r);
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public List<Resume> getAllSorted() {
+        List<Resume> sorted = new ArrayList<>(storage.values());
+        sorted.sort(Resume.RESUME_COMPARATOR);
+        return sorted;
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+}
