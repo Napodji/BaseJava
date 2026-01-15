9972742cdf585fcbec03f04102a7e9c1add12217
Napoji <sasha.k7@mail.ru>
2026-01-16 02:06:47 +0300

    Add fullName parameter to Resume constructor

--- src/com/basejava/webapp/model/Resume.java
+++ src/com/basejava/webapp/model/Resume.java
@@ -1,24 +1,41 @@
 package com.basejava.webapp.model;
 
+import java.util.Comparator;
+import java.util.Objects;
 import java.util.UUID;
 
 /**
  * Initial resume class
  */
 public class Resume implements Comparable<Resume> {
-
     private final String uuid;
+    private final String fullName;
+
+    public static final Comparator<Resume> RESUME_COMPARATOR =
+            Comparator.comparing(Resume::getFullName)
+                    .thenComparing(Resume::getUuid);
 
     public Resume() {
-        this(UUID.randomUUID().toString());
+        this(UUID.randomUUID().toString(), "");
     }
 
     public Resume(String uuid) {
+        this(uuid, "");
+    }
+
+    public Resume(String uuid, String fullName) {
+        Objects.requireNonNull(uuid, "uuid can not be null");
+        Objects.requireNonNull(fullName, "fullName can not be null");
         this.uuid = uuid;
+        this.fullName = fullName;
     }
 
     public String getUuid() {
         return uuid;
+    }
+
+    public String getFullName() {
+        return fullName;
     }
 
     @Override
@@ -26,21 +43,21 @@ public class Resume implements Comparable<Resume> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Resume resume = (Resume) o;
-        return uuid.equals(resume.uuid);
+        return uuid.equals(resume.uuid) && fullName.equals(resume.fullName);
     }
 
     @Override
     public int hashCode() {
-        return uuid.hashCode();
+        return Objects.hash(uuid, fullName);
     }
 
     @Override
     public String toString() {
-        return uuid;
+        return "Resume{uuid='" + uuid + "', fullName='" + fullName + "'}";
     }
 
     @Override
     public int compareTo(Resume o) {
-        return uuid.compareTo(o.uuid);
+        return RESUME_COMPARATOR.compare(this, o);
     }
 }
