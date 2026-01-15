088f94b1eb640d6e71a0f1940d9d0f84464e9b99
Napoji <sasha.k7@mail.ru>
2026-01-16 02:20:43 +0300

    Update ListStorage with getAllSorted

--- src/com/basejava/webapp/storage/ListStorage.java
+++ src/com/basejava/webapp/storage/ListStorage.java
@@ -13,8 +13,10 @@ public class ListStorage extends AbstractStorage {
     }
 
     @Override
-    public Resume[] getAll() {
-        return storage.toArray(new Resume[0]);
+    public List<Resume> getAllSorted() {
+        List<Resume> sorted = new ArrayList<>(storage);
+        sorted.sort(Resume.RESUME_COMPARATOR);
+        return sorted;
     }
 
     @Override
