f5cdd463fc7796fedc1a060e2ca7a61e38d207e4
Napoji <sasha.k7@mail.ru>
2026-01-29 01:35:45 +0300

    Add TextSection for PERSONAL and OBJECTIVE sections

---
+++ src/com/basejava/webapp/model/TextSection.java
@@ -0,0 +1,21 @@
+package com.basejava.webapp.model;
+
+import java.util.Objects;
+
+public class TextSection extends AbstractSection {
+    private final String content;
+
+    public TextSection(String content) {
+        Objects.requireNonNull(content, "content must not be null");
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public String toString() {
+        return content;
+    }
+}
