dc5a975d2ac8460341b5d2980a175ebc3656332a
Napoji <sasha.k7@mail.ru>
2026-02-11 15:36:26 +0300

    hw_8: add AbstractFileStorage, add cause to StorageException

--- src/com/basejava/webapp/exception/StorageException.java
+++ src/com/basejava/webapp/exception/StorageException.java
@@ -1,18 +1,21 @@
 package com.basejava.webapp.exception;
 
-/**
+/**.
  * Base exception for storage errors
  */
 public class StorageException extends RuntimeException {
-
     private final String uuid;
 
     public StorageException(String message) {
-        this(message, null);
+        this(message, null, null);
     }
 
     public StorageException(String message, String uuid) {
-        super(message);
+        this(message, uuid, null);
+    }
+
+    public StorageException(String message, String uuid, Exception e) {
+        super(message, e);
         this.uuid = uuid;
     }
 
