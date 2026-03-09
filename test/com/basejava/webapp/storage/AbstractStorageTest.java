6e2dd12c5a2167b5f58257d4196948e3967eb510
Napoji <sasha.k7@mail.ru>
2026-03-10 00:17:34 +0300

    hw_9: fix code style in AbstractStorageTest

--- test/com/basejava/webapp/storage/AbstractStorageTest.java
+++ test/com/basejava/webapp/storage/AbstractStorageTest.java
@@ -13,8 +13,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public abstract class AbstractStorageTest {
-    protected static final File STORAGE_DIR =
-            new File("storage");
+    protected static final File STORAGE_DIR = new File("storage");
     protected Storage storage;
 
     private static final String UUID_1 = "uuid1";
